9d11af11041b41cfa39e1dde94371b4b8a33fa91
katvengo <katvengo@gmail.com>
2019-08-06 19:35:12 -0700

    ru

--- APIs/services/ticketmasterDataService.js
+++ APIs/services/ticketmasterDataService.js
@@ -4,9 +4,11 @@ const moment = require('moment')
 
 
 var defaultTime = moment(Date.now()).format('YYYY[-]MM[-]DDTHH:mm:ss').toString(); 
-console.log(defaultTime)
+console.log('***************** Default time:', defaultTime)
 function searchTicketmaster(query, time = defaultTime) {
   const ticketmasterUrl = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${query}&countryCode=US&localStartDateTime=${time}&apikey=h0unNltRv7LnmEOS2kYZ43GR9GKBQjCC`;
+
+  console.log('****************** TicketMaster URL: ', ticketmasterUrl);
        return axios.get(ticketmasterUrl)
            //.then(transformData)
            .catch((err) =>{
