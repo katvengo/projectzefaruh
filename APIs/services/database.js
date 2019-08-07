4c7beb57bd9e78d429b49ca5452eb7c542cf9d85
katvengo <katvengo@gmail.com>
2019-08-06 18:51:38 -0700

    committing API stuff

--- APIs/services/database.js
+++ APIs/services/database.js
@@ -6,8 +6,14 @@ const moment = require('moment')
 var defaultTime = moment(Date.now()).format('DDTHH:mm:ss').toString(); 
 // console.log(defaultTime)
 function searchThruDatabase(query, location, date, time) {
-  const databaseURL = `/api/events/:${query}/:${location}/:${date}/:${time}`;
-       return axios.get(databaseURL)
+       return axios.get("/events/", {
+         params: {
+          query: query,
+          // location: location,
+          // date: date,
+          // time: time
+         }
+       })
            .catch((err) =>{
            console.log(err)   
            })
@@ -17,5 +23,6 @@ function searchThruDatabase(query, location, date, time) {
           // .then(transformData)
           
 }
+//console.log(searchThruDatabase('rollerblade'))
 //console.log("transforming" + transformData)
 module.exports = searchThruDatabase
