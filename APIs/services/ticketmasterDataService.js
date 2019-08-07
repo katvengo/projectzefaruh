1059c30f338ed845969bc360d4cbebe3ca5f0e12
katvengo <katvengo@gmail.com>
2019-08-07 08:57:59 -0700

    Transformed ticketMaster Data

--- APIs/services/ticketmasterDataService.js
+++ APIs/services/ticketmasterDataService.js
@@ -1,20 +1,28 @@
 //const transformData = require('./ticketmasterToZafaraTransformation');
 const axios = require('axios');
 const moment = require('moment')
+const transformData = require('./ticketmasterToZafaraTransformation');
 
 
 var defaultTime = moment(Date.now()).format('YYYY[-]MM[-]DDTHH:mm:ss').toString(); 
-console.log('***************** Default time:', defaultTime)
+//console.log('***************** Default time:', defaultTime)
 function searchTicketmaster(query, time = defaultTime) {
   const ticketmasterUrl = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${query}&countryCode=US&localStartDateTime=${time}&apikey=h0unNltRv7LnmEOS2kYZ43GR9GKBQjCC`;
 
-  console.log('****************** TicketMaster URL: ', ticketmasterUrl);
+  //console.log('****************** TicketMaster URL: ', ticketmasterUrl);
        return axios.get(ticketmasterUrl)
-           //.then(transformData)
+           .then(function (apiEvents){
+            apiEventsParsed = apiEvents.data._embedded.events 
+            console.log(apiEventsParsed)
+           var sendData = apiEventsParsed.map(transformData)
+           console.log(sendData)
+             //return apiEventsParsed
+            return sendData;
+           })
            .catch((err) =>{
            console.log(err)   
            })
-             
+          
 }
 
 module.exports = searchTicketmaster
